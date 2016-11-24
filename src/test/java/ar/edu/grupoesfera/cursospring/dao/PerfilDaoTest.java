3b1445573b0463089d994bfbf411c3b297d1472a
rodrigo1990 <mcd77.1990@gmail.com>
2016-11-24 07:01:24 -0300

    Test perfilDao
    2/3 perfil dao test, CUIDADO ! estube navegando editarPerfil y me andubo
    a veces.
    Por otro lado, en los testeos tube resultados dispares ante un Usuario
    editado que mantiene el mismo nombre que el original.
    Las demas posibilidades no las testie ya que esa de entrada daba mal ,
    peguenle una chekeada y backapeen por las dudas !
    
    saludos !
    Las demas posibilidades no las probe hay algo raro ahi

---
+++ src/test/java/ar/edu/grupoesfera/cursospring/dao/PerfilDaoTest.java
@@ -0,0 +1,104 @@
+package ar.edu.grupoesfera.cursospring.dao;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import ar.edu.grupoesfera.cursospring.modelo.Banda;
+import ar.edu.grupoesfera.cursospring.modelo.SpringTest;
+import ar.edu.grupoesfera.cursospring.modelo.Usuario;
+import static org.assertj.core.api.Assertions.*;
+
+public class PerfilDaoTest extends SpringTest {
+	
+	@Inject
+	PerfilDAOService perfilDaoService;
+	
+	//@Test
+	@Rollback @Transactional
+	public void testEditarPerfil(){
+		
+		Usuario usuario = new Usuario();
+		
+		//usuario.setIdusuario(1);
+		usuario.setNombre("username");
+		usuario.setPass("hola");
+		usuario.setEmail("mail@user.com");
+		usuario.setInstrumento("bajo");
+		usuario.setLocalidad("San Justo");
+		usuario.setPartido("La Matanza");
+		usuario.setProvincia("Buenos Aires");
+		usuario.setInfluencias("banda");
+		usuario.setImagen("img");
+		usuario.setFechaNacimiento(null);
+		
+		
+		getSession().save(usuario);
+		
+		Usuario usuarioEditado = new Usuario();
+		
+	
+		usuarioEditado.setNombre("username");
+		usuarioEditado.setPass("holaasd");
+		usuarioEditado.setEmail("mailasdasd@user.com");
+		usuarioEditado.setInstrumento("bajista");
+		usuarioEditado.setLocalidad("San Justo5");
+		usuarioEditado.setPartido("La Matanza4");
+		usuarioEditado.setProvincia("Buenos Airesa");
+		usuarioEditado.setInfluencias("banda2");
+		
+		
+		Usuario usuarioActualizado=perfilDaoService.editarPerfil(usuarioEditado, usuario.getNombre());
+		
+		assertThat(usuarioEditado).isEqualTo(usuarioActualizado);
+		
+		
+		
+	}
+	
+	@Test
+	@Rollback @Transactional
+	public void obtenerBandaPorNombreTest(){
+		
+		Banda banda = new Banda();
+		banda.setGenero("rock");
+		banda.setNombre("banda");
+		
+		getSession().save(banda);
+		
+		Banda banda2=new Banda();
+		banda2=perfilDaoService.obtenerBandaPorNombre(banda.getNombre());
+		
+		assertThat(banda).isEqualTo(banda2);
+	}
+	
+	public void obtenerUsuarioPorNombre(){
+		
+		Usuario usuario = new Usuario();
+		usuario.setNombre("username");
+		usuario.setPass("hola");
+		usuario.setEmail("mail@user.com");
+		usuario.setInstrumento("bajo");
+		usuario.setLocalidad("San Justo");
+		usuario.setPartido("La Matanza");
+		usuario.setProvincia("Buenos Aires");
+		usuario.setInfluencias("banda");
+		usuario.setImagen("img");
+		usuario.setFechaNacimiento(null);
+		
+		getSession().save(usuario);
+		
+		Usuario usuarioObtenido = new Usuario();
+		
+		usuarioObtenido=perfilDaoService.obtenerUsuarioPorNombre(usuario.getNombre());
+		
+		assertThat(usuario).isEqualTo(usuarioObtenido);
+		
+	}
+
+}
